312d2cc9c51bb34de3daea3946fb4098f0d9e490
oatsu <panchi.psp@gmail.com>
2020-08-24 00:20:49 +0900

    Update setup_wsl_1.sh

--- sh/setup_wsl_1.sh
+++ sh/setup_wsl_1.sh
@@ -1,7 +1,6 @@
 # Ubuntu の更新
 cd ~
 sudo sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu/%http://ftp.iij.ad.jp/pub/linux/ubuntu/archive/%g" /etc/apt/sources.list
-sudo apt install python-is-python3
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove
 
 # .bashrc に書き込み
